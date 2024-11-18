4c05a0feee8d03cc4684de4ebc79e3418b629a54
shogo-takao <taka.sho2508@gmail.com>
2024-11-18 23:23:31 +0900

    fix

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,6 @@ import api from '@/api/$api'
 import mock from '@/api/$mock'
 import aspidaClient from '@aspida/fetch'
 
-export const ApiClient =
-  process.env.NODE_ENV === 'development' ? mock(aspidaClient()) : api(aspidaClient())
+// export const ApiClient =
+//   process.env.NODE_ENV === 'development' ? mock(aspidaClient()) : api(aspidaClient())
+export const ApiClient = mock(aspidaClient())
